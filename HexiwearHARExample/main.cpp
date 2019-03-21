eb57b880fbc99be6a8abcef07c0b0d85679f4459
Dennis Shim <hisroar@gmail.com>
2019-03-20 17:51:00 -0700

    Put data sampling in a loop

--- HexiwearHARExample/main.cpp
+++ HexiwearHARExample/main.cpp
@@ -24,94 +24,101 @@ FXAS21002 gyro(PTC11,PTC10);
 // (note the calls to Thread::wait below for delays)
 int main() {
 
-    int i = 0;
+	while(1) {
 
-    // clock variables
-    std::clock_t mid = std::clock();
-    float duration;
-    
-    // Configure Accelerometer FXOS8700, Magnetometer FXOS8700
-    accel.accel_config();
-    //mag.mag_config();
-    gyro.gyro_config();
+	    int i = 0;
 
-    string LABELS[] = {"WALKING",
-                       "WALKING_UPSTAIRS",
-                       "WALKING_DOWNSTAIRS",
-                       "SITTING",
-                       "STANDING",
-                       "LAYING"};
+	    // clock variables
+	    std::clock_t mid = std::clock();
+	    float duration;
+	    
+	    // Configure Accelerometer FXOS8700, Magnetometer FXOS8700
+	    accel.accel_config();
+	    //mag.mag_config();
+	    gyro.gyro_config();
 
-    float accel_data[3];
-    float gyro_data[3];
+	    string LABELS[] = {"WALKING",
+	                       "WALKING_UPSTAIRS",
+	                       "WALKING_DOWNSTAIRS",
+	                       "SITTING",
+	                       "STANDING",
+	                       "LAYING"};
 
-    float gravity[3] = {0.0, 0.0, 0.0};
-    float alpha = 0.8;
-    
-    float input_data[128*6];
+	    float accel_data[3];
+	    float gyro_data[3];
 
-    printf("DNN_BEGIN: Begin Data Acquisition from sensors....\r\n\r\n");
-    wait(0.5);
-    printf("DEBUG1\r\n");
-    
-    while (i < 200) {
-        mid = std::clock();
-        accel.acquire_accel_data_g(accel_data);
-        gyro.acquire_gyro_data_dps(gyro_data);
+	    float gravity[3] = {0.0, 0.0, 0.0};
+	    float alpha = 0.8;
+	    
+	    float input_data[128*6];
 
-        gravity[0] = alpha * gravity[0] + (1 - alpha) * accel_data[0];
-        gravity[1] = alpha * gravity[1] + (1 - alpha) * accel_data[1];
-        gravity[2] = alpha * gravity[2] + (1 - alpha) * accel_data[2];
+	    printf("DNN_BEGIN: Begin Data Acquisition from sensors....\r\n\r\n");
+	    wait(0.1);
+	    //printf("DEBUG1\r\n");
+	    
+	    while (i < 200) {
+	        mid = std::clock();
+	        accel.acquire_accel_data_g(accel_data);
+	        gyro.acquire_gyro_data_dps(gyro_data);
 
-        if(i == 72) {
-            printf("Now.\n\n");
-        }
+	        gravity[0] = alpha * gravity[0] + (1 - alpha) * accel_data[0];
+	        gravity[1] = alpha * gravity[1] + (1 - alpha) * accel_data[1];
+	        gravity[2] = alpha * gravity[2] + (1 - alpha) * accel_data[2];
 
-        if(i >= 72) {
-            input_data[(i-72)*6+0] = (accel_data[0] - gravity[0]);
-            input_data[(i-72)*6+1] = (accel_data[1] - gravity[1]);
-            input_data[(i-72)*6+2] = (accel_data[2] - gravity[2]);
+	        if(i == 72) {
+	            //printf("Now.\n\n");
+	        }
 
-            input_data[(i-72)*6+3] = gyro_data[0]/100.0;
-            input_data[(i-72)*6+4] = gyro_data[1]/100.0;
-            input_data[(i-72)*6+5] = gyro_data[2]/100.0;
+	        if(i >= 72) {
+	            input_data[(i-72)*6+0] = (accel_data[0] - gravity[0]);
+	            input_data[(i-72)*6+1] = (accel_data[1] - gravity[1]);
+	            input_data[(i-72)*6+2] = (accel_data[2] - gravity[2]);
 
-            printf("%d: x %f y %f z %f r %f p %f y %f\n",i,
-                   accel_data[0] - gravity[0],accel_data[1] - gravity[1],accel_data[2] - gravity[2],
-                   gyro_data[0],gyro_data[1],gyro_data[2]);
-        }
+	            input_data[(i-72)*6+3] = gyro_data[0]/100.0;
+	            input_data[(i-72)*6+4] = gyro_data[1]/100.0;
+	            input_data[(i-72)*6+5] = gyro_data[2]/100.0;
 
-        // compute how long computations and printf took and subtract it from wait
-        duration = (std::clock() - mid) / (float) CLOCKS_PER_SEC;
-        // only wait if computation time is less than wait time
-        if(duration < 0.02) {
-            wait(0.02 - duration);
-        }
+	            printf("%d: x %f y %f z %f r %f p %f y %f\n",i,
+	                   accel_data[0] - gravity[0],accel_data[1] - gravity[1],accel_data[2] - gravity[2],
+	                   gyro_data[0],gyro_data[1],gyro_data[2]);
+	        }
 
-        i++;
-    }
-    
+	        // compute how long computations and printf took and subtract it from wait
+	        duration = (std::clock() - mid) / (float) CLOCKS_PER_SEC;
+	        // only wait if computation time is less than wait time
+	        if(duration < 0.02) {
+	            wait(0.02 - duration);
+	        }
 
-    printf("Data Acquisition done.\n\n");
+	        i++;
+	    }
+	    
 
-    for(i = 0; i < 128; i++) {
-        printf("%d: x %f y %f z %f r %f p %f y %f\n",i,
-               input_data[i*6+0],input_data[i*6+1],input_data[i*6+2],
-               input_data[i*6+3],input_data[i*6+4],input_data[i*6+5]);
-    }
+	    printf("Data Acquisition done.\n\n");
+	    
+		/*
+	    for(i = 0; i < 128; i++) {
+	        printf("%d: x %f y %f z %f r %f p %f y %f\n",i,
+	               input_data[i*6+0],input_data[i*6+1],input_data[i*6+2],
+	               input_data[i*6+3],input_data[i*6+4],input_data[i*6+5]);
+	    }
+	    */
 
-    Context ctx;  //creating the context class, the stage where inferences take place 
-    // wrapping the input data in a tensor class
-    Tensor* input_x = new WrappedRamTensor<float>({1, 128*6}, (float*) input_data);
+	    Context ctx;  //creating the context class, the stage where inferences take place 
+	    // wrapping the input data in a tensor class
+	    Tensor* input_x = new WrappedRamTensor<float>({1, 128*6}, (float*) input_data);
 
-    get_dnn6_2_ctx(ctx, input_x);  // pass the tensor to the context
-    S_TENSOR pred_tensor = ctx.get("y_pred:0");  // getting a reference to the output tensor
-    ctx.eval(); //trigger the inference
+	    get_dnn6_2_ctx(ctx, input_x);  // pass the tensor to the context
+	    S_TENSOR pred_tensor = ctx.get("y_pred:0");  // getting a reference to the output tensor
+	    ctx.eval(); //trigger the inference
 
-    int pred_label = *(pred_tensor->read<int>(0, 0));  //getting the result back
-    printf("Predicted label: ");
-    printf(LABELS[pred_label].c_str());
-    printf("\n\n");
+	    int pred_label = *(pred_tensor->read<int>(0, 0));  //getting the result back
+	    printf("Predicted label: ");
+	    printf(LABELS[pred_label].c_str());
+	    printf("\n\n");
+
+	    wait(0.2);
+	}
 
     return 0;
 }
